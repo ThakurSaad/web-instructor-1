81b2008ee612871d41520f457d790eb7e3b12635
thakurSaad <thakursaad613@gmail.com>
2022-07-01 00:25:53 +0600

    billing management table

---
+++ src/Pages/Home/BillingRow.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BillingRow = () => {
+  return (
+    <tr>
+      <td>Billing Id</td>
+      <td>Full Name</td>
+      <td>Email</td>
+      <td>Phone</td>
+      <td>Paid Amount</td>
+      <td></td>
+    </tr>
+  );
+};
+
+export default BillingRow;
--- src/Pages/Home/Layout.js
+++ src/Pages/Home/Layout.js
@@ -1,8 +1,24 @@
 import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading";
 import AddBillModal from "./AddBillModal";
+import BillingRow from "./BillingRow";
 
 const Layout = () => {
   const [addBillModal, setAddBillModal] = useState(null);
+
+  const {
+    data: billingList,
+    isLoading,
+    refetch,
+  } = useQuery("billing-list", () =>
+    fetch("http://localhost:5000/billing-list").then((res) => res.json())
+  );
+
+  // console.log(billingList);
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <main>
@@ -33,6 +49,33 @@ const Layout = () => {
             </label>
             {addBillModal && <AddBillModal />}
           </div>
+        </div>
+      </section>
+      {/* bill-management-table */}
+      <section className="bill-management-table lg:mx-8">
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th>Billing Id</th>
+                <th>Full Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Paid Amount</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {billingList.map((billingSingle, index) => (
+                <BillingRow
+                  key={index}
+                  index={index}
+                  billingSingle={billingSingle}
+                  refetch={refetch}
+                />
+              ))}
+            </tbody>
+          </table>
         </div>
       </section>
     </main>
