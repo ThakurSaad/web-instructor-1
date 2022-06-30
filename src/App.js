7d77c8eee1edd33cb70400b47b870ca2a99fc41c
thakurSaad <thakursaad613@gmail.com>
2022-06-30 16:36:42 +0600

    toggle between login and register form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Register from "./Pages/Login/Register";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="home" element={<Home></Home>}></Route>
         <Route path="register" element={<Register></Register>}></Route>
+        <Route path="login" element={<Login></Login>}></Route>
       </Routes>
       <ToastContainer />
     </div>
---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  // react hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // firebase hooks
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  // variable
+  let errorElement;
+
+  const onSubmit = async (data) => {
+    signInWithEmailAndPassword(data.email, data.password);
+  };
+
+  return (
+    <section className="min-h-screen flex justify-center items-center">
+      <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-5xl font-bold text-center text-primary">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text font-semibold pl-3">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your email"
+                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: "Provide a valid email",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.email?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text font-semibold pl-3">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your password"
+                className="input border-r-0 border-l-0 border-t-0 border-b-2 border-primary rounded-none w-full max-w-xs"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is required",
+                  },
+                  minLength: {
+                    value: 6,
+                    message: "Must be more than 5 characters",
+                  },
+                })}
+              />
+              <label className="label">
+                {errors.password?.type === "required" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            {errorElement}
+            <input
+              className="btn btn-primary w-full max-w-xs"
+              type="submit"
+              value="Login"
+            />
+          </form>
+          <p className="p-1">
+            <small>
+              New user?{" "}
+              <Link className="text-primary font-semibold" to="/register">
+                Register
+              </Link>
+            </small>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
