041cfd101b8f5a86f8a7078a4f801fa309ade706
thakurSaad <thakursaad613@gmail.com>
2022-06-30 15:02:32 +0600

    daisyui plugin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn">Button</button>
     </div>
   );
 }
