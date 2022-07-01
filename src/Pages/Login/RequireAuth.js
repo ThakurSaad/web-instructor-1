04d5448468cd5b89411e382ee7e4e2e3f23b0a36
thakurSaad <thakursaad613@gmail.com>
2022-07-01 18:56:56 +0600

    login issue solved

--- src/Pages/Login/RequireAuth.js
+++ src/Pages/Login/RequireAuth.js
@@ -2,16 +2,18 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../../firebase.init";
+import useToken from "../../hooks/useToken";
 import Loading from "../Shared/Loading";
 
 function RequireAuth({ children }) {
   const [user, loading] = useAuthState(auth);
   let location = useLocation();
+  const [token] = useToken(auth);
 
   if (loading) {
     return <Loading></Loading>;
   }
-  if (!user) {
+  if (!user || !token) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
