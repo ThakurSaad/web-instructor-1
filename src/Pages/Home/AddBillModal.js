b66615430f937ded40bb675253e81c9bdb35b754
thakurSaad <thakursaad613@gmail.com>
2022-07-01 11:27:41 +0600

    form validation done

--- src/Pages/Home/AddBillModal.js
+++ src/Pages/Home/AddBillModal.js
@@ -2,10 +2,9 @@ import React from "react";
 import { toast } from "react-toastify";
 
 const AddBillModal = ({ submitMethod, _id }) => {
-  console.log(submitMethod);
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    // variables
     const fullName = event.target.fullName.value;
     const email = event.target.email.value;
     const phone = event.target.phone.value;
@@ -16,47 +15,54 @@ const AddBillModal = ({ submitMethod, _id }) => {
       phone: phone,
       paidAmount: paidAmount,
     };
+    const emailRegEx = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email);
 
-    if (submitMethod === "POST") {
-      fetch("http://localhost:5000/add-billing", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(bill),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.acknowledged) {
-            toast.success("Submitted Successfully");
-            window.location.reload();
-          } else {
-            toast.error(
-              "An error occurred. Please reload the page and try again"
-            );
-          }
-        });
-    }
+    if (phone.length !== 11) {
+      toast.error("Phone number must be 11 characters long");
+    } else if (!emailRegEx) {
+      toast.error("Please provide a valid email");
+    } else {
+      if (submitMethod === "POST") {
+        fetch("http://localhost:5000/add-billing", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(bill),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged) {
+              toast.success("Submitted Successfully");
+              // window.location.reload();
+            } else {
+              toast.error(
+                "An error occurred. Please reload the page and try again"
+              );
+            }
+          });
+      }
 
-    if (submitMethod === "PUT") {
-      fetch(`http://localhost:5000/update-billing/${_id}`, {
-        method: "PUT",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(bill),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.acknowledged) {
-            toast.success("Edited Successfully");
-            window.location.reload();
-          } else {
-            toast.error(
-              "An error occurred. Please reload the page and try again"
-            );
-          }
-        });
+      if (submitMethod === "PUT") {
+        fetch(`http://localhost:5000/update-billing/${_id}`, {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(bill),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.acknowledged) {
+              toast.success("Edited Successfully");
+              window.location.reload();
+            } else {
+              toast.error(
+                "An error occurred. Please reload the page and try again"
+              );
+            }
+          });
+      }
     }
   };
 
@@ -81,9 +87,6 @@ const AddBillModal = ({ submitMethod, _id }) => {
                 className="input input-bordered w-80"
                 required
               />
-              <label className="label">
-                <span className="label-text-alt">{}</span>
-              </label>
             </div>
 
             <div className="form-control w-full max-w-xs">
@@ -97,9 +100,6 @@ const AddBillModal = ({ submitMethod, _id }) => {
                 className="input input-bordered w-80"
                 required
               />
-              <label className="label">
-                <span className="label-text-alt">{}</span>
-              </label>
             </div>
 
             <div className="form-control w-full max-w-xs">
@@ -113,9 +113,6 @@ const AddBillModal = ({ submitMethod, _id }) => {
                 className="input input-bordered w-80"
                 required
               />
-              <label className="label">
-                <span className="label-text-alt">{}</span>
-              </label>
             </div>
 
             <div className="form-control w-full max-w-xs">
@@ -129,11 +126,8 @@ const AddBillModal = ({ submitMethod, _id }) => {
                 className="input input-bordered w-80"
                 required
               />
-              <label className="label">
-                <span className="label-text-alt">{}</span>
-              </label>
             </div>
-            <div className="modal-action my-0">
+            <div className="modal-action">
               <input
                 type="submit"
                 value="Submit"
